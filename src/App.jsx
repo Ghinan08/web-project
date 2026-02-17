07da1fd44931727001907876f7a7d194cc3f066b
Ghinan08 <alwaysongaming10@gmail.com>
2026-02-17 17:22:47 +0700

    Setup awal frontend react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Frontend Ready!</h1>
+      <p>Halaman ini siap diintegrasikan dengan Backend.</p>
+    </div>
+  );
+}
+
+export default App;
